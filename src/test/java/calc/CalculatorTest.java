4e233570fbfaea891359694760db46b7d7a591ed
codeanddonuts <kkw200@naver.com>
2019-08-09 21:45:45 +0900

    refactor: Calculator test function names and api from JUnit to AssertJ

--- src/test/java/calc/CalculatorTest.java
+++ src/test/java/calc/CalculatorTest.java
@@ -3,88 +3,82 @@ package calc;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 public class CalculatorTest {
     @Test
-    void 덧셈테스트() {
-        assertThat(new Calculator("1 +    2 + 3").getResult()).isEqualTo(6.0);
+    void additionTest() {
+        assertThat(
+                new Calculator("  1 +    2 + 3").result()
+        ).isEqualTo(6.0);
     }
 
     @Test
-    void 뺄셈테스트() {
-        assertThat(new Calculator("1 -   3").getResult()).isEqualTo(-2.0);
+    void subtractionTest() {
+        assertThat(
+                new Calculator("1 -   3").result()
+        ).isEqualTo(-2.0);
     }
 
     @Test
-    void 곱셈테스트() {
-        assertThat(new Calculator("1   * -3").getResult()).isEqualTo(-3.0);
+    void multiplicationTest() {
+        assertThat(
+                new Calculator("1   * -3").result()
+        ).isEqualTo(-3.0);
     }
 
     @Test
-    void 나눗셈테스트A() {
-        assertThat(new Calculator("12 /   3").getResult()).isEqualTo(4.0);
+    void divisionTest() {
+        assertThat(
+                new Calculator("  12 /   3").result()
+        ).isEqualTo(4.0);
     }
 
     @Test
-    void 나눗셈테스트B() {
-        assertThrows(Exception.class, () -> {
-            new Calculator("11 /   0");
-        });
+    void divisionExceptionTestA() {
+        assertThatThrownBy(() -> new Calculator("11 /   0"));
     }
 
     @Test
-    void 나눗셈테스트C() {
-        assertThrows(Exception.class, () -> {
-            new Calculator("0        /   0");
-        });
+    void divisionExceptionTestB() {
+        assertThatThrownBy(() -> new Calculator("0        /   0"));
     }
 
     @Test
-    void 나눗셈테스트D() {
-        assertThrows(Exception.class, () -> {
-            new Calculator("1 /  0 + 1");
-        });
+    void divisionExceptionTestC() {
+        assertThatThrownBy(() -> new Calculator("1 /  0 + 1"));
     }
 
     @Test
-    void 복합수식테스트() {
-        assertThat(new Calculator("1 - 3 * 6 + 6 / 2").getResult()).isEqualTo(-3.0);
+    void expressionTest() {
+        assertThat(
+                new Calculator("1 - 3 * 6 + 6 / 2").result()
+        ).isEqualTo(-3.0);
     }
 
     @Test
-    void 예외테스트A() {
-        assertThrows(Exception.class, () -> {
-            new Calculator("-");
-        });
+    void exceptionTestA() {
+        assertThatThrownBy(() -> new Calculator("-"));
     }
 
     @Test
-    void 예외테스트B() {
-        assertThrows(Exception.class, () -> {
-            new Calculator("1 +");
-        });
+    void exceptionTestB() {
+        assertThatThrownBy(() -> new Calculator("1 +"));
     }
 
     @Test
-    void 예외테스트C() {
-        assertThrows(Exception.class, () -> {
-            new Calculator("1 + /");
-        });
+    void exceptionTestC() {
+        assertThatThrownBy(() -> new Calculator("1 + /"));
     }
 
     @Test
-    void 예외테스트D() {
-        assertThrows(Exception.class, () -> {
-            new Calculator("1       +  5  /");
-        });
+    void exceptionTestD() {
+        assertThatThrownBy(() -> new Calculator("1       +  5  /"));
     }
 
     @Test
-    void 예외테스트E() {
-        assertThrows(Exception.class, () -> {
-            new Calculator("1 3 5");
-        });
+    void exceptionTestE() {
+        assertThatThrownBy(() -> new Calculator("1 3 5"));
     }
 }
